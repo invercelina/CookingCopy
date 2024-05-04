176c60b903b6f35ac602582689a36100c9ff6d2e
invercelina <invercelina@gmail.com>
2024-05-05 00:39:21 +0900

    타이머 준비중

--- app/src/main/java/com/example/cooking/MainActivity.java
+++ app/src/main/java/com/example/cooking/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cooking;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +24,23 @@ public class MainActivity extends AppCompatActivity {
         EditText etminute = findViewById(R.id.et_minute);
         EditText etsecond = findViewById(R.id.et_second);
 
+        TextView tvhour = findViewById(R.id.tv_hour);
+        TextView tvminute = findViewById(R.id.tv_minute);
+        TextView tvsecond = findViewById(R.id.tv_second);
 
+        Button startbtn = findViewById(R.id.btn_start);
 
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ethour.setVisibility(View.INVISIBLE);
+                etminute.setVisibility(View.INVISIBLE);
+                etsecond.setVisibility(View.INVISIBLE);
+
+                tvhour.setVisibility(View.VISIBLE);
+                tvminute.setVisibility(View.VISIBLE);
+                tvsecond.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
