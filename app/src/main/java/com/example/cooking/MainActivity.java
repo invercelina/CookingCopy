6e3bcec2a4a0898b64ba123eb1fe4271b89107a8
invercelina <invercelina@gmail.com>
2024-05-05 10:33:31 +0900

    시분초 각각 TextView에 숫자 넣기

--- app/src/main/java/com/example/cooking/MainActivity.java
+++ app/src/main/java/com/example/cooking/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
         Button startbtn = findViewById(R.id.btn_start);
 
+        int hour;
+        int minute;
+        int second;
+
         startbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,7 +45,18 @@ public class MainActivity extends AppCompatActivity {
                 tvhour.setVisibility(View.VISIBLE);
                 tvminute.setVisibility(View.VISIBLE);
                 tvsecond.setVisibility(View.VISIBLE);
+
+                inputNumber(ethour, tvhour);
+                inputNumber(etminute, tvminute);
+                inputNumber(etsecond, tvsecond);
             }
         });
+
     }
+
+    public void inputNumber(EditText editText, TextView textView) {
+        int number = Integer.parseInt(editText.getText().toString());
+        textView.setText(String.valueOf(number));
+    }
+
 }
